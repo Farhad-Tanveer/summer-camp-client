b285ad078b5f0263599321824dac929b7cf2d400
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-06-07 11:28:58 +0200

    added navbar

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../../public/logo.png";
 
 const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link>Instructors</Link>
+      </li>
+      <li>
+        <Link>Classes</Link>
+      </li>
+      <li>
+        <Link>Dashboard</Link>
+      </li>
+    </>
+  );
   return (
     <div>
-      <h2>this is navbar</h2>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <Link to="/">
+            <a className="btn btn-ghost normal-case text-xl">
+              <img className=" w-16" src={logo} alt="" />
+              <span className=" text-3xl hidden lg:block font-bold text-[#422C18]">
+                Sporty Summer
+              </span>
+            </a>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-xl font-serif font-light text-gray-500">
+            {navOptions}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn bg-[#848C2F] hover:bg-[#6f752b] text-white">
+            Register now
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
