d36c7f1154980e205de1658d76f6ef708d197078
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-06-08 16:19:38 +0200

    added custome hook useAdmin to check is it admin or not

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -48,8 +49,21 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
-      setUser(loggedUser);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("http://localhost:3000/jwt", { email: currentUser.email })
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+
       setLoading(false);
     });
     return () => {
